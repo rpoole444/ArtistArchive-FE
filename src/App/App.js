71bea329b3732df7a2466a243ef1d0fdc7e8c493
Reid Poole <poole.reid@gmail.com>
2023-02-28 21:06:17 -0600

    Import apicalls method, create stateful hooks and bring assign apicall to artist's variable

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Header/Header";
+import Library from "../ArtistLibrary/ArtistLibrary";
+import { retrieveAllArtists } from "../apiCalls";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [artists, setArtists] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getArtists = () => {
+    setLoading(true);
+    retrieveAllArtists().then((data) => {
+      setArtists(data.artists);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getArtists();
+  }, []);
+  console.log(artists);
   return (
     <main className="App">
       <Header />
+      <Library allArtists={artists} />
     </main>
   );
-}
+};
 
 export default App;
