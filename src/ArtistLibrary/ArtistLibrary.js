43279050e4b5625b29f48b92e5e0ebe036bd2a44
Reid Poole <poole.reid@gmail.com>
2023-02-28 20:39:06 -0600

    Add component attributes and iterator that creates individual cards

---
+++ src/ArtistLibrary/ArtistLibrary.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ArtistLibrary.css";
+import ArtistCard from "../ArtistCard/ArtistCard";
+
+const ArtistLibrary = ({ allArtists }) => {
+  const mappedArtists =
+    allArtists.length > 0 ? (
+      allArtists.map((artist) => {
+        return (
+          <ArtistCard
+            id={artist.id}
+            key={artist.id}
+            image={artist.image}
+            name={artist.name}
+            rank={artist.rank}
+          />
+        );
+      })
+    ) : (
+      <div className="no-artist">No Artists Matched Your Search</div>
+    );
+  return <div className="artist-container">{mappedArtists}</div>;
+};
+
+export default ArtistLibrary;
