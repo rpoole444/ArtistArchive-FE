b6cf99d0ac9cabc663b0b96708281fce9c0fd0f3
Reid Poole <poole.reid@gmail.com>
2023-03-01 12:43:32 -0600

    Add ArtistCard style and classname adjustments

--- src/ArtistCard/ArtistCard.js
+++ src/ArtistCard/ArtistCard.js
@@ -5,7 +5,7 @@ import "./ArtistCard.css";
 const ArtistCard = ({ id, image, name, genre, rank }) => {
   return (
     <section className="card-container">
-      <h2>{name}</h2>
+      <h2 className="artist-name-tag">{name}</h2>
       <img className="card-image" src={image} alt={`${name} - musician`} />
       <div className="card-styler">
         <p className="card-genre">{`Genre: ${genre}`}</p>
