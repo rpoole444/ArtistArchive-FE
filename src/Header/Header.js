3566a11ca643a5ec7d75f4de2559fabe15da62dd
Reid Poole <poole.reid@gmail.com>
2023-03-01 21:49:45 -0600

    Add Header NavLinks, need to style still

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ location }) => {
+  let navLink;
+  if (location === "favorites") {
+    navLink = (
+      <Link className="landing-page-link" to="/">
+        <h1>Return Home »</h1>
+      </Link>
+    );
+  } else {
+    navLink = (
+      <Link className="favorites-link" to="/favorites">
+        <h1>Your Favorites »</h1>
+      </Link>
+    );
+  }
   return (
     <nav className="header">
-      <img className="record-icon" src="turntable.png" alt="turntable" />
+      <Link to="/">
+        <img className="record-icon" src="turntable.png" alt="turntable" />
+      </Link>
       <h1 className="app-title">Artist Archive</h1>
       <input
         className="search"
