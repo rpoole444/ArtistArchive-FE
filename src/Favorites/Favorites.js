eeebdc5b0d686b0052c0967f890f1e14b9a39d40
Reid Poole <poole.reid@gmail.com>
2023-03-03 11:52:52 -0600

    Use props favorites from APP to rendor the artists on Fave's page

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,24 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ArtistLibrary from "../ArtistLibrary/ArtistLibrary";
-import { getAllFavorites } from "../apiCalls";
-import { cleanArtistData } from "../utilities";
 
 const Favorites = (props) => {
-  // const [favorites, setFavorites] = useState([]);
   const [error, setError] = useState("");
-
-  // useEffect(() => {
-  //   getFavorites();
-  // });
-
-  // const getFavorites = () => {
-  //   getAllFavorites()
-  //     .then((data) => {
-  //       const cleandedFavorites = data.map((artist) => cleanArtistData(artist));
-  //       setFavorites(cleandedFavorites);
-  //     })
-  //     .catch((error) => setError({ error: error.message }));
-  // };
 
   return (
     <>
