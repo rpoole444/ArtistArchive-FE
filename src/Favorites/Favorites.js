3d69dfb7a82af632cab98a3216872425c6448320
Reid Poole <poole.reid@gmail.com>
2023-03-03 22:07:58 -0600

    Add styling and classnames to Favorites component

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -1,18 +1,19 @@
 import React from "react";
 import ArtistLibrary from "../ArtistLibrary/ArtistLibrary";
 import PropTypes from "prop-types";
+import "./Favorites.css";
 
 const Favorites = (props) => {
   const { favorites } = props;
   return (
-    <>
+    <div className="favorites-container">
       {!favorites.length && (
         <h2 className="empty-favorites">
           You haven't favorited any musicians yet!
         </h2>
       )}
       <ArtistLibrary allArtists={favorites} />
-    </>
+    </div>
   );
 };
 
